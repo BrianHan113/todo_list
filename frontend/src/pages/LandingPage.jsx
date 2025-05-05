c2b9302d96b833f2bb5aa659a3aaa27aa41f0369
Brian Han <shan790@aucklanduni.ac.nz>
2025-05-05 23:43:39 +1200

    Login pass ui

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -5,11 +5,15 @@ const LandingPage = () => {
 
   const words = ["Get", "It", "Done."];
 
+  const onLoginClick = () => {
+    console.log("Clicked");
+  }
+
   useGSAP(() => {
     gsap.fromTo(
       ".hero-text h1",
       { y: 50, opacity: 0 }, /* From */
-      { y: 0, opacity: 1, stagger: 0.8, duration: 1, ease: "power2.inOut" } /* To */
+      { y: 0, opacity: 1, stagger: 0.6, duration: 1, ease: "power2.inOut" } /* To */
     );
   });
 
@@ -19,10 +23,31 @@ const LandingPage = () => {
         <div className="w-[65vw] h-[100vh]" />
         <div className="w-[35vw] h-[100vh] bg-amber-600 " >
           <div className="flex flex-col justify-center items-center h-screen">
-            <div className="space-y-2 text-9xl font-bold text-white hero-text">
+            <div className="text-9xl font-bold text-white select-none hero-text">
               {words.map((word, i) => (
                 <h1 key={i}>{word}</h1>
               ))}
+            </div>
+            <div className="text-1xl font-bold text-white mt-10 select-none">
+              <form className="flex flex-col space-y-4 w-[80vw] sm:w-96">
+                <input
+                  type="text"
+                  placeholder="Username"
+                  autoFocus
+                  className="p-3 border rounded-md focus:outline-none"
+                />
+                <input
+                  type="password"
+                  placeholder="Password"
+                  className="p-3 border rounded-md focus:outline-none"
+                />
+                <button onClick={onLoginClick} className="bg-yellow-500 text-white font-bold py-3 rounded-md">
+                  Login
+                </button>
+                <p className="text-sm text-center text-white select-none">
+                  Don't have an account? <a href="#" className="underline text-blue-600">Sign up</a>
+                </p>
+              </form>
             </div>
           </div>
         </div>
