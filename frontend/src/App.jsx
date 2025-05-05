20754d4efad305362b3f81d1db3b550f9d2a32c3
Brian Han <shan790@aucklanduni.ac.nz>
2025-05-05 21:42:37 +1200

    init frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import LandingPage from './pages/LandingPage.jsx'
+import MainApp from './pages/MainApp.jsx'
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/app" element={<MainApp />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ frontend/src/pages/MainApp.jsx
@@ -0,0 +1,7 @@
+const MainApp = () => {
+    return (
+        <h1>Main App</h1>
+    );
+}
+
+export default MainApp
