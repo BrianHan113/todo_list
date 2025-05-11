9b149c28f681a9d0e1999a2ce1348efdc65d3023
Brian Han <shan790@aucklanduni.ac.nz>
2025-05-12 02:02:43 +1200

    tooltip clicking func

--- frontend/src/components/Tooltip.jsx
+++ frontend/src/components/Tooltip.jsx
@@ -1,16 +1,41 @@
-import { useState } from 'react';
+import { useState, useEffect, useRef } from 'react';
 
-const Tooltip = ({ text, tooltipText }) => {
+const Tooltip = ({ text, tooltipText, y_translate }) => {
   const [hovered, setHovered] = useState(false);
+  const [clicked, setClicked] = useState(false);
+  const tooltipRef = useRef(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      // See if click was inside or outside of our tooltip box
+      if (tooltipRef.current && !tooltipRef.current.contains(event.target)) {
+        setClicked(false);
+      }
+    };
+
+    if (clicked) { // Tooltip is open
+      document.addEventListener('mousedown', handleClickOutside); // Callback function whenever we click
+    }
+
+    return () => { // Tooltip is closed
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [clicked]); // Only activates at first mount, and when we click the tooltip opener text
 
   return (
-    <div style={{ position: 'relative', display: 'inline-block' }}
+    <div
+      ref={tooltipRef}
+      style={{ position: 'relative', display: 'inline-block' }}
       onMouseEnter={() => setHovered(true)}
       onMouseLeave={() => setHovered(false)}
+      onClick={() => setClicked(c => !c)}
     >
       <p className="underline text-blue-600 cursor-pointer">{text}</p>
-      {hovered && (
-        <span className="absolute bg-gray-800 text-white p-1.5 rounded-md top-[-450%] left-1/2 transform -translate-x-1/2 whitespace-normal break-words w-72 z-10">
+      {(hovered || clicked) && (
+        <span
+          className="absolute bg-gray-800 text-white p-1.5 rounded-md left-1/2 transform -translate-x-1/2 whitespace-normal break-words w-72 z-10"
+          style={{ top: `${y_translate}%` }}
+        >
           {tooltipText}
         </span>
       )}
--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -86,19 +86,19 @@ const LandingPage = () => {
             </div>
             <div className="hidden md:block font-bold text-white ">
               <p className="mt-5 text-sm text-center text-white select-none">
-                Don't have an account? <a href="#" className="underline text-blue-600">Sign up</a>
+                Don't have an account? <a href="#" className="underline text-blue-500">Sign up</a>
               </p>
               <p className="mt-2 text-sm text-center text-white select-none">
-                <Tooltip text="Get What Done?" tooltipText={description} />
+                <Tooltip text="Get What Done?" tooltipText={description} y_translate={-475} />
               </p>
             </div>
           </div>
           <div className="block md:hidden font-bold text-white ">
-            <p className="mt-5 text-sm text-center text-white select-none">
-              Don't have an account? <a href="#" className="underline text-blue-600">Sign up</a>
+            <p className="mt-5 text-lg text-center text-white select-none">
+              Don't have an account? <a href="#" className="underline text-blue-500">Sign up</a>
             </p>
-            <p className="mt-2 text-sm text-center text-white select-none">
-              <Tooltip text="Get What Done?" tooltipText={description} />
+            <p className="mt-2 text-lg text-center text-white select-none">
+              <Tooltip text="Get What Done?" tooltipText={description} y_translate={-550} />
             </p>
           </div>
         </div>
