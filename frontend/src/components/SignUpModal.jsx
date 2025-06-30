362d826a37979dce4a25cd96b2332354a4fdfcf7
Brian Han <shan790@aucklanduni.ac.nz>
2025-06-30 16:04:01 +1200

    add signup to mobile

--- frontend/src/components/SignUpModal.jsx
+++ frontend/src/components/SignUpModal.jsx
@@ -17,7 +17,7 @@ export default function SignUpModal() {
 
       {open && (
         <div className="fixed inset-0 flex items-center justify-center bg-black/50 z-40">
-          <div className="bg-amber-600 p-6 rounded-lg w-full max-w-md">
+          <div className="bg-amber-600 p-6 rounded-lg w-full max-w-sm">
             <h2 className="text-xl font-semibold mb-4">Create an Account</h2>
 
             <form className="flex flex-col space-y-4 w-full">
@@ -44,8 +44,6 @@ export default function SignUpModal() {
                 Cancel
               </button>
             </form>
-
-
 
           </div>
         </div>
--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -100,12 +100,7 @@ const LandingPage = () => {
           </div>
           <div className="block md:hidden font-bold text-white ">
             <p className="mt-5 text-lg text-center text-white select-none">
-              Don't have an account? {" "}
-              <span
-                onClick={handleSignUpClick}
-                className="underline text-blue-600 cursor-pointer">
-                Sign up
-              </span>
+              <SignUpModal />
             </p>
             <p className="mt-2 text-lg text-center text-white select-none">
               <Tooltip text="Get What Done?" tooltipText={description} y_translate={-550} />
