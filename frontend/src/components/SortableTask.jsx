f1786115dcfaf1d9d0ab171241535d0b10ad00c4
Brian Han <shan790@aucklanduni.ac.nz>
2025-07-02 23:05:28 +1200

    tweaks

--- frontend/src/components/SortableTask.jsx
+++ frontend/src/components/SortableTask.jsx
@@ -38,9 +38,27 @@ const SortableTask = ({ id, task, onDelete, onClick }) => {
             e.stopPropagation();
             onDelete();
           }}
-          className="text-red-500 hover:underline ml-4 cursor-pointer px-2 py-1"
+          className="ml-4 cursor-pointer px-2 py-1 group"
         >
-          Delete
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="35"
+            height="35"
+            viewBox="0 0 48 48"
+          >
+            <path
+              className="fill-[#f44336] group-hover:fill-red-700 transition-colors"
+              d="M44,24c0,11.045-8.955,20-20,20S4,35.045,4,24S12.955,4,24,4S44,12.955,44,24z"
+            ></path>
+            <path
+              className="fill-white"
+              d="M29.656,15.516l2.828,2.828l-14.14,14.14l-2.828-2.828L29.656,15.516z"
+            ></path>
+            <path
+              className="fill-white"
+              d="M32.484,29.656l-2.828,2.828l-14.14-14.14l2.828-2.828L32.484,29.656z"
+            ></path>
+          </svg>
         </button>
       </div>
     </li>
