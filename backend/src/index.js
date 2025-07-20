e46c1859730d2547a62b68e99eac3fb10b4422dd
Brian Han <brianhan113@gmail.com>
2025-07-20 16:10:49 +1200

    added basic security middleware

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,15 +2,24 @@ import express from "express"
 import cors from "cors"
 import dotenv from "dotenv"
 import pool from "./config/db.js"
+import helmet from "helmet"
+import xss from "xss-clean"
+import rateLimiter from "express-rate-limit"
 
 dotenv.config()
 
 const app = express()
 const port = process.env.PORT || 5000
 
+
 // Middleware
 app.use(express.json())
 app.use(cors())
+app.use(helmet())
+app.use(rateLimiter({
+  windowMs: 15*60*1000, // 15 mins
+  max: 100, // max num reqs per windowMs
+}))
 
 // Routes
 
@@ -18,8 +27,12 @@ app.use(cors())
 
 // Debug/testing
 app.get("/", async (req, res) => {
+  try {
     const result = await pool.query("SELECT current_database()")
     res.send(`DB name is: ${result.rows[0].current_database}`)
+  } catch {
+    res.send("Failed to connect to db")
+  }
 })
 
 // Run Server
