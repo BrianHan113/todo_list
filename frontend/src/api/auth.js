5799de1bdef4ccb803939abc38aa40f43d0f8df6
Brian Han <brianhan113@gmail.com>
2025-07-23 23:42:41 +1200

    add .env for easier config

--- frontend/src/api/auth.js
+++ frontend/src/api/auth.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:5000/api/v1";
+const BASE_URL = import.meta.env.VITE_API_URL;
 
 export const login = async (username, password) => {
   const res = await fetch(`${BASE_URL}/auth/login`, {
