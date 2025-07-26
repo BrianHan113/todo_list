be7376061ebdbf62c8ffcf6f8846f35eaf32c976
Brian Han <brianhan113@gmail.com>
2025-07-26 21:55:13 +1200

    samesite none for prod

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -7,7 +7,7 @@ const createCookie = (res, token) => {
 	res.cookie("token", token, {
 		httpOnly: true,
 		secure: true,
-		sameSite: "Lax",
+		sameSite: 'None',
 		maxAge: 12 * 60 * 60 * 1000,
 	});
 };
